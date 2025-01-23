6bf75d28a4f2e91872920956ef22714932c2186d
Lane <lanedwofford@gmail.com>
2025-01-23 10:46:27 -0600

    Add operate function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Calculator
 
+let num1;
+let operator;
+let num2;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -15,4 +19,21 @@ function multiply(num1, num2) {
 // TODO: Add error handling for division by zero
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+    }
 }
