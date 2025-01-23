a59b1d738bdcf5dc3221f6393935513b64280cc2
Lane <lanedwofford@gmail.com>
2025-01-23 10:42:53 -0600

    Add basic arithmetic functions

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Calculator
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// TODO: Add error handling for division by zero
+function divide(num1, num2) {
+    return num1 / num2;
+}
